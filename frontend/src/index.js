7f822415cebb42102b635a77b152a36c39f8410a
Eric Callari <ericcallari1@gmail.com>
2024-03-30 18:04:15 -0700

    hides google api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Set Google API Key to global variable
+window.REACT_APP_GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
