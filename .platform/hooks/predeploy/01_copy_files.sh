7233a7233d94242911fe9138ce72f81a8d648b2d
Eric Callari <ericcallari1@gmail.com>
2025-02-10 17:29:56 -0800

    debugging launch to aws

--- .platform/hooks/predeploy/01_copy_files.sh
+++ .platform/hooks/predeploy/01_copy_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cp -r /var/app/staging/backend/* /var/app/staging/
+echo "Starting predeploy hook script"
+echo "Contents of /var/app/staging before copy:"
+ls -la /var/app/staging
+echo "Contents of /var/app/staging/backend:"
+ls -la /var/app/staging/backend
+echo "Copying files..."
+cp -rv /var/app/staging/backend/* /var/app/staging/
+echo "Removing backend directory..."
 rm -rf /var/app/staging/backend
+echo "Final contents of /var/app/staging:"
+ls -la /var/app/staging
